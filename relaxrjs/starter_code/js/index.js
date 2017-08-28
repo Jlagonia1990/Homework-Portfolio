cd6487809ea15642238c8a31e66df4bb71f7494b
Jlagonia1990 <30609167+Jlagonia1990@users.noreply.github.com>
2017-08-28 13:16:32 -0400

    first commit

---
+++ relaxrjs/starter_code/js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+function readmore() {
+  $('.readmore').hide();
+  $('#show-this-on-click').slideDown();
+  $('.readless hide').show();
+}
+function readless hide() {
+  $('.readless hide').hide();
+  $('#show-this-on-click').slideUp();
+  $('.readmore').show();
+}
+
+function sidebarLearnMore() {
+  $('.learnmore').hide();
+  $('#learnmoretext').slideDown();
+}
+});
+
